9db36f37246e370221767fcd3381a8c19d6f53be
jmingrove21 <min_k21@naver.com>
2019-01-16 16:28:42 +0900

    Default Setting Client, Server

--- SocketProgramming/Program.cs
+++ SocketProgramming/Program.cs
@@ -5,13 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-
 namespace SocketProgramming
 {
-    class Program
+    class Program//server Section
     {
+        public static string dateTime ="["+DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")+"]";
+        public static int port_number = 12345;
         static void Main(string[] args)
         {
+            new Program();
+        }
+        public Program()
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port_number);
+            server.Bind(ipep);
+            server.Listen(10);
+
+            Console.WriteLine(dateTime+"[Msg] Server Start...");
+            Console.WriteLine(dateTime+"[Msg] Port number : "+port_number);
+
+            Socket client = server.Accept();
+            IPEndPoint ip = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine(dateTime + "[Connect] Connect by " + ip.Address);
+
+            String buf_str = "Connect Server Success!";
+            Byte[] buf = Encoding.UTF8.GetBytes(buf_str);
+            client.Send(buf);
+            Byte[] rec_buf = new Byte[1024];
+            client.Receive(rec_buf);
+            String rec_str = Encoding.Default.GetString(rec_buf);
+            Console.WriteLine(dateTime + "[recieve] " + rec_str);
+
+            client.Close();
+            server.Close();
         }
     }
+
 }
---
+++ SocketProgramming_Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace SocketProgramming_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+        public Program()
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            client.Connect(ipep);
+            Console.WriteLine("Socket Connect!");
+            Byte[] buf = new Byte[1024];
+            client.Receive(buf);
+            String rec_buf = Encoding.Default.GetString(buf);
+            Console.WriteLine(rec_buf);
+
+            String send_str = "Send messeage by client";
+            Byte[] send_buf = new Byte[1024];
+            send_buf = Encoding.UTF8.GetBytes(send_str);
+            client.Send(send_buf);
+
+            client.Close();
+            
+        }
+    }
+}
